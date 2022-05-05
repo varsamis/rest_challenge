405ccf4876fe3eb8e8b1255621c498660e6a2ca6
Varsamis Karavarsamis <varsamisk@medallia.com>
2022-05-05 10:57:49 +0200

    Add RotatingFileHandler for logging.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 """
 Chalenge restfull API
 """
+import logging
+from logging.handlers import RotatingFileHandler
 import asyncio
 import structlog
 
@@ -14,7 +16,31 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-log = structlog.get_logger()
+def setup_logging():
+    # Get the Logger
+    _logger = logging.getLogger(__file__)
+    
+    # Set logging level
+    _logger.setLevel(logging.INFO)
+    
+    # Configure log handler
+    rotating_log_handler = RotatingFileHandler('/tmp/testLogFile.log', maxBytes=10000, backupCount=1)
+    
+    # Add RotatingFileHandler to the logger
+    _logger.addHandler(rotating_log_handler)
+    
+    # Configure Structlog
+    struct_logger = structlog.wrap_logger(
+        _logger,
+        wrapper_class=structlog.BoundLogger,
+        context_class=dict,
+        cache_logger_on_first_use=None,
+        logger_factory_args=None
+    )
+
+    return struct_logger
+
+log = setup_logging()
 
 # Dependency
 def get_db():
@@ -26,29 +52,28 @@ def get_db():
 
 @app.get('/report/{report_id}')
 def read_report(report_id: int, db: Session = Depends(get_db)):
-    log.msg('GET /report/', report_id=report_id)
+    log.info('GET /report/', report_id=report_id)
     db_report = crud.get_report(db, report_id=report_id)
     if db_report is None:
         raise HTTPException(status_code=404, detail="Report not found")
-    log.msg('Response /report/', db_report=db_report)
+    log.info('Response /report/', db_report=db_report)
     return db_report
 
 
 @app.post("/report/new")
 def create_report(file: UploadFile, db: Session = Depends(get_db)):
-    log.msg('POST /report/new', file=file.filename)
+    log.info('POST /report/new', file=file.filename)
     if file.filename.split('.')[-1] not in ['csv', 'json']:
         raise HTTPException(status_code=415, detail='Unsupported Media Type')
     result = asyncio.run(helpers.process_file(file))
     db_report = crud.create_report(db, report=schemas.ReportCreate(file_name=file.filename, result=result))
-    log.msg('Response /report/new', db_report=db_report)
+    log.info('Response /report/new', db_report=db_report)
     return db_report
 
 
 @app.get('/reports', response_model=list[schemas.Report])
 def read_reports(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    log.msg('GET /reports')
+    log.info('GET /reports')
     reports = crud.get_reports(db, skip=skip, limit=limit)
-    log.msg('GET /reports', reports_len=len(reports))
+    log.info('GET /reports', reports_len=len(reports))
     return reports
-    
